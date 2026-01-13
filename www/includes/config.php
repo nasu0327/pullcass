eef7b0563e5079b2f03f5e1474d4958fa270cd3a
nasu0327 <106227912+nasu0327@users.noreply.github.com>
2026-01-13 13:35:33 +0900

    Warning修正: 定数の二重定義を解消

--- www/includes/config.php
+++ www/includes/config.php
@@ -12,9 +12,5 @@ define('DB_USER', 'root');
 define('DB_PASS', 'nasu0327');
 define('DB_CHARSET', 'utf8mb4');
 
-// 環境設定
-define('APP_ENV', 'production'); // development or production
-define('APP_DEBUG', false);
-
 // URL設定
 define('APP_URL', 'http://54.95.76.111');
