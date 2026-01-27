4caae30f588f15ee9100276f0ed452a7ca139c88
nasu0327 <106227912+nasu0327@users.noreply.github.com>
2026-01-27 18:33:12 +0900

    Fix path to bootstrap.php in cast_widgets

--- www/app/manage/cast_widgets/index.php
+++ www/app/manage/cast_widgets/index.php
@@ -1,5 +1,5 @@
 <?php
-require_once __DIR__ . '/../../includes/bootstrap.php';
+require_once __DIR__ . '/../../../includes/bootstrap.php';
 require_once __DIR__ . '/../includes/auth.php';
 
 // ログイン認証チェック
